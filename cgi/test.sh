84c2e5cf5401d012cf073c4781654cc951fb381d
Calvin - Desktop <claverty@calpoly.edu>
2018-02-06 14:23:44 -0800

    From unix server

--- cgi/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo -n -e "Content-Type: text/html\r\n"
-echo -n -e "Foo-Header: bar\r\n"
-echo -n -e "\r\n"
-echo -n -e "<HTML><HEAD><TITLE>Test CGI Script</TITLE></HEAD>\r\n"
-echo -n -e "<BODY>Simple CGI Test!</BODY></HTML>\r\n"
